3187656dc52a755a2f206b965ecefe277f24e286
Newton001 <Newtonkelvin75@gmail.com>
2020-09-24 13:07:42 +0300

    Set ALLOWED_HOST

--- user/settings.py
+++ user/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'rn0r45p-*qcvbg(&$ms7r8u#91h+a^9ci0m+av9mc*1wnz677p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
